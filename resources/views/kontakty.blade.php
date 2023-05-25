3ca9567cb41d49963b57c93a002b556a751403f8
Evgeniy <260443@mail.ru>
2023-05-25 15:11:07 +0500

    Kontakty page html + css

--- resources/views/kontakty.blade.php
+++ resources/views/kontakty.blade.php
@@ -17,21 +17,61 @@
 </div>
 
 <div class="kontakty">
-  <div class="page-title">Контакты</div>
-
   <div class="content-wrapper">
-
-    <p>Общество с ограниченной ответственностью - ООО "Уральские Семена"</p>
-    <p>Адрес: 456300 Россия, Челябинская область, город Миасс, Объездная дорога 4/48, офис 3.</p>
-    <p>Телефон: +7 (902) 614-09-67</p>
-    <p>E-mail: info@semena-darom.ru</p>
-    <p>ИНН: 7415099134</p>
-    <p>ОГРН: 1177456098434</p>
-    <p>КПП: 741501001</p>
-    <p>Расчетный счет: 40702810372000021827</p>
-    <p>ПАО "Сбербанк": 454048, г. Челябинск, ул. Энтузиастов, 9а.</p>
-    <p>БИК банка: 047501602</p>
-    <p>Кор счет: 30101810700000000602</p>
+    <div class="kontakty-title">Контакты</div>
+    <div class="company-name">Общество с ограниченной ответственностью - ООО "Уральские Семена"</div>
+    <div class="flex-container">
+      <div class="kontakty-item">
+        <div class="kontakty-item__title">Адрес</div>
+        <div class="kontakty-item__text">
+          <img src="/img/contacts-geolocation.svg" alt="">
+          <span>456300 Россия, Челябинская область, город Миасс, Объездная дорога 4/48, офис 3.</span>
+          </div>
+      </div>
+      <div class="kontakty-item">
+        <div class="kontakty-item__title">Телефон</div>
+        <div class="kontakty-item__text">
+          <img src="/img/contacts-phone.svg" alt="">
+          <span>+7 (902) 614-09-67</span>
+        </div>
+      </div>
+      <div class="kontakty-item">
+        <div class="kontakty-item__title">Эл. почта</div>
+        <div class="kontakty-item__text">
+          <img src="/img/contacts-email.svg" alt="">
+          <span>info@semena-darom.ru</span>
+        </div>
+      </div>
+    </div>
+    <div class="kontakty-subtitle">Реквизиты</div>
+    <div class="flex-container">
+      <div class="kontakty-item">
+        <div class="kontakty-item__title">ОГРН</div>
+        <div class="kontakty-item__text">41177456098434</div>
+      </div>
+      <div class="kontakty-item">
+        <div class="kontakty-item__title">ИНН</div>
+        <div class="kontakty-item__text">7415099134</div>
+      </div>
+      <div class="kontakty-item">
+        <div class="kontakty-item__title">КПП</div>
+        <div class="kontakty-item__text">741501001</div>
+      </div>
+    </div>
+    <div class="flex-container mb60">
+      <div class="kontakty-item">
+        <div class="kontakty-item__title">ПАО "Сбербанк"</div>
+        <div class="kontakty-item__text">454048, г. Челябинск, ул. Энтузиастов, 9а.</div>
+      </div>
+      <div class="kontakty-item">
+        <div class="kontakty-item__title">Расчетный счет</div>
+        <div class="kontakty-item__text">40702810372000021827</div>
+      </div>
+      <div class="kontakty-item">
+        <div class="kontakty-item__title">Кор счет</div>
+        <div class="kontakty-item__text">30101810700000000602</div>
+      </div>
+    </div>
 
     <div class="map">
       <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A81307a1b8cec7e06bf9f9d518352f7464f896c561e2a2eb27539dacfcda47122&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
